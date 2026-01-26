ec071ad211a76a916757d56e088b278abb32b790
Whytecrowe <dor7an@gmail.com>
2026-01-26 15:43:44 -0800

    add basic tests to main.rs

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use saga_core::model::Echo;
+use chrono::{ Local };
+use uuid::Uuid;
+
+
+fn main() {
+    println!("<=== Test ===>");
+
+    // create an Echo
+    let echo = Echo::new(
+        Local::now().date_naive(),
+        Uuid::new_v4(),
+        "First Echo: Hello World!".to_string(),
+    );
+
+    println!("Echo created: {:#?}", echo);
+    println!("Echo char count: {}", echo.char_count());
+
+    // Test mutable borrow
+    let mut echo2 = echo.clone();
+    println!("Echo2 text initial: {:?}", echo2.markdown);
+    echo2.update_markdown("Second Echo: Updated!".to_string());
+    println!("Echo2 new text: {:?}", echo2.markdown);
+
+    // Test consumption with conversion
+    let markdown = echo2.into_markdown();
+    println!("Echo2 consumed: {:?}", markdown);
+
+    // date display
+    println!("Echo display day: {}", echo.display_day());
+}
